6b2ed6963fcea4ab19c7e91922dae271f4f43c62
EduExtreme <edugexp@gmail.com>
2024-08-12 21:16:04 -0400

    feat: initial commit

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen w-full items-center justify-center bg-black text-white">
+      <h1 className="text-xl font-semibold text-blue-600">Hello Extreme</h1>
+    </main>
+  );
+}
